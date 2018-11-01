4d297a4d84b05917ac2ad7f5ba3aa30b78eb8ae6
Szabó András <szabag@gmail.com>
2018-11-01 23:26:14 +0100

    setup logger

--- src/main/java/com/wikidata/sport/Application.java
+++ src/main/java/com/wikidata/sport/Application.java
@@ -1,5 +1,7 @@
 package com.wikidata.sport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +10,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.wikidata.sport")
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.info("Application starting..");
+
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/wikidata/sport/Controllers/TestController.java
+++ src/main/java/com/wikidata/sport/Controllers/TestController.java
@@ -1,5 +1,8 @@
 package com.wikidata.sport.Controllers;
 
+import com.wikidata.sport.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @ComponentScan("")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+
     @GetMapping("/hello")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        logger.info("Hello " + name);
         model.addAttribute("name", name);
         return "hello";
     }
